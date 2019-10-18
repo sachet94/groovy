882cd694378d90f0fe5a9849bcfd8eb9e40ac256
sachet <sachetshambharkar@pratititech.com>
2019-10-18 12:37:07 +0530

    v1

---
+++ new/Variable.groovy
@@ -0,0 +1,2 @@
+def name = 'sachet'
+println name
---
+++ new/first.groovy
@@ -0,0 +1,8 @@
+package com.test.demo  
+
+class MyFirstGroovyProject {
+
+    static void main(args) {
+        println('hello fellas');
+    }
+}
