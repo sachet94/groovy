e70702b2cf926982e504bd8af9576fc5ed20283f
sachet <sachetshambharkar@pratititech.com>
2019-10-18 13:47:40 +0530

    v2

---
+++ new/Data_types.groovy
@@ -0,0 +1,10 @@
+//byte
+byte b = 10
+println b
+//or
+def byte c = 59
+println c  
+
+//To check minimun and maximum range for byte 
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
--- new/Variable.groovy
+++ new/Variable.groovy
@@ -1,2 +1,11 @@
 def name = 'sachet'
-println name
+def (String x, int y, double z) = [38,59,62]
+def (a,b,c) = [30,55,60] //assigning multiple variable
+println 'Name is ' +name //how to assign variable
+println "Baap is ${name}" //how to assign variable
+println a
+println b 
+println c 
+println x
+println y
+println z
